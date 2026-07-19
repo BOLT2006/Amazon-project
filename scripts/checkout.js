204d241e2658face03d069f1b5b32b4dcdfbae27
Yash Sathe <yashsathe1357@gmail.com>
2026-07-19 11:22:46 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,31 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/backend-practise.js'
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value 1');
+  });
+}).then((value) => {
+    return new Promise((resolve) =>{
+            loadCart(() =>{
+                resolve();
+            });
+    });                       
+}).then (() =>{
+    
+  renderOrderSummary();
+  renderPaymentSummary();                        
+});
+/*
 loadProducts(() => {
+    loadCart(() =>{
+        
   renderOrderSummary();
   renderPaymentSummary();
+
+    });
 });
+*/
