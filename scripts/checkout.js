0c71f8204ee201759ff89d92a3831307d15d5f57
Yash Sathe <yashsathe1357@gmail.com>
2026-07-19 11:46:19 +0530

    Convert loadProduct from Xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/backend-practise.js'
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 
